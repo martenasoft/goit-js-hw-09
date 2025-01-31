69653fb5c6ac97433d8d9a56c78e9b63777005d9
Anisimov Kostya <k.anisimov@epicentrk.ua>
2025-01-31 19:17:53 +0200

    FC

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+const formData = {
+  email: '',
+  message: ''
+};
+
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const saveForm = (element) => {
+  const fromElement = element.target;
+  if (fromElement.tagName === undefined || !['INPUT', 'TEXTAREA'].includes(fromElement.tagName)) {
+    return;
+  }
+
+  const value = fromElement.value.trim();
+  formData[fromElement.name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+const loadForm = (form) => {
+  const data = localStorage.getItem(STORAGE_KEY);
+
+  if (data === null) {
+    return;
+  }
+
+  const parsedData = JSON.parse(data);
+  if (parsedData.email === undefined || parsedData.message === undefined) {
+    return;
+  }
+
+  form.email.value = parsedData.email;
+  form.message.value = parsedData.message;
+}
+
+const submitForm = (event) => {
+  event.preventDefault();
+  const form = event.target;
+
+  if (form.email.value === '' || form.message.value === '') {
+    console.warn("Fill please all fields");
+    return;
+  }
+
+  form.message.value = form.email.value = '';
+  localStorage.removeItem(STORAGE_KEY);
+
+};
+
+export function handleFrom(form) {
+  loadForm(form);
+  form.addEventListener('input', saveForm);
+  form.addEventListener('submit', submitForm)
+}
